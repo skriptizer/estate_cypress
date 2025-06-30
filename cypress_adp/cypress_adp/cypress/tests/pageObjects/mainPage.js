b3b5086cd47768225e742477480850266121861e
Ivan Voronov <skriptizer@gmail.com>
2025-06-30 23:48:48 +0500

    Update mainPage.js

--- cypress_adp/cypress_adp/cypress/tests/pageObjects/mainPage.js
+++ cypress_adp/cypress_adp/cypress/tests/pageObjects/mainPage.js
@@ -3,55 +3,13 @@ const XPATH = require('../../main/locators/baseLocatorChildren/XPATH');
 const Button = require('../../main/elements/baseElementChildren/button');
 
 class MainPage extends BaseForm {
-  #OGPOButton;
-
-  #mutualButton;
-
-  #kaskoButton;
-
-  #MSTButton;
-
-  #VMSButton;
-
-  #quoteButton;
-
   #estateButton;
 
   constructor() {
     super(new XPATH('//div[@class="ant-card-body"]'), 'main page');
-    this.#OGPOButton = new Button(new XPATH('//a[@href="/ogpo/create"]'), 'OGPO button');
-    this.#mutualButton = new Button(new XPATH('//a[@href="/mutual"]'), 'Mutual button');
-    this.#MSTButton = new Button(new XPATH('//a[@href="/mst/create"]'), 'MST button');
-    this.#kaskoButton = new Button(new XPATH('//a[@href="/kasko/create"]'), 'kasko button');
-    this.#VMSButton = new Button(new XPATH('//a[@href="/vms/create"]'), 'VMS button');
-    this.#quoteButton = new Button(new XPATH('//a[@href="/quotes"]'), 'Quote button');
     this.#estateButton = new Button(new XPATH('//a[@href="/estate/create"]'), 'estate button');
   }
-
-  clickOGPOButton() {
-    this.#OGPOButton.clickElement();
-  }
-
-  clickMutualButton() {
-    this.#mutualButton.clickElement();
-  }
-
-  clickKaskoButton() {
-    this.#kaskoButton.clickElement();
-  }
-
-  clickMSTButton() {
-    this.#MSTButton.clickElement();
-  }
-
-  clickVMSButton() {
-    this.#VMSButton.clickElement();
-  }
-
-  clickQuoteButton() {
-    this.#quoteButton.clickElement();
-  }
-
+  
   clickEstateButton() {
     this.#estateButton.clickElement();
   }
